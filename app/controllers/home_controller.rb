a117147b721d47e53e045eebdc7bfd4dae7bc051
Cesar Rodriguez <cesarodriguez4@gmail.com>
2020-03-04 22:55:19 -0400

    processing docx file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,41 @@
+require 'docx'
+require 'open-uri'
+
+# /[.]{2,}/
 class HomeController < ApplicationController
   def index
+    @content = []
+    buffer = open('https://drive.google.com/uc?export=download&id=1To7UEYG5XImyD5vDinc3zk3yNU2xYXq7')
+    doc = Docx::Document.open(buffer)
+    process_file(doc) if session[:csv]
+  end
+
+  def create
+    csv_text = File.read(params[:csv])
+    # Save it in session
+    session[:csv] = csv_text
+    redirect_to :home_index
+  end
+
+  # Use some helper method in order to make this code simplier
+  def process_file(doc)
+    csv_values = session[:csv].split(',')
+    doc.paragraphs.each do |p|
+      p.each_text_run do |tr|
+        ## Array sobre cada texto, varias veces para modificar cada ocurrencia
+        new_text = tr.to_s
+        new_arr = []
+        new_text.split.collect do |str|
+          if /[…]{2,}|[.]{2,}/.match?(str)
+            new_arr << str.sub(/[…]{2,}|[.]{2,}/, csv_values[0])
+            csv_values.shift(1)
+          else
+            new_arr << str
+          end
+        end
+        tr.substitute(tr.to_s, new_arr.join(' '))
+      end
+    end
+    doc.save('document.docx')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get 'home/index'
+  post 'home/index', to: 'home#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
